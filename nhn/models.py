3713289c9ed0d3a10816f9b7ee6a9f60399324a1
jihye0420 <20181054@sungshin.ac.kr>
2023-01-20 14:25:01 +0900

    MOD: DB 컬럼 수정

--- nhn/models.py
+++ nhn/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    url = models.URLField(verbose_name='게시물 URL')
+    url = models.URLField(primary_key=True, verbose_name='게시물 URL')
     title = models.CharField(max_length=255, verbose_name='게시물 제목')
     published_datetime = models.DateTimeField(verbose_name='게시물 발행일(작성일자)')
-    body = models.TextField(verbose_name='게시물 발행일(작성일자)')
+    body = models.TextField(verbose_name='게시물 HTML')
     attachment_list = models.TextField(verbose_name='첨부파일 이름 리스트')
 
     class Meta:

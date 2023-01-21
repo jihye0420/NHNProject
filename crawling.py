fbebe25cc5764dd36ae602013a508e9d16e541d2
jihye0420 <20181054@sungshin.ac.kr>
2023-01-22 01:42:38 +0900

    ADD: 크롤링 데이터 insert test 완료

--- crawling.py
+++ crawling.py
@@ -1,43 +1,51 @@
 import os
-import ssl
-import traceback
-import urllib.parse
-import urllib.request
 import re
-from datetime import datetime, timedelta
+import traceback
+from datetime import datetime
 
 # from pytz import timezone
 import pytz
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 import django
 
 django.setup()
+
 from nhn.models import Post
 
 # 파일 쓰기 (print문)
 import sys
+
 sys.stdout = open('stdout.txt', 'w', encoding='utf-8')
+
+
+def get_driver():
+    # 구글은 분당 4회이상 접근 허용하지 않기 때문에 sleep을 길게 준다.
+    # time.sleep(100)
+
+    # local chrome path
+    # path = "/usr/share/AISpera/merge_crawler/chromedriver"  # >> 나의 로컬 환경:/homejhhwang/updateWork/PIT/chromedriver vs 서버 환경:/usr/share/AISpera/google_news/chromedriver"
+    chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(options=chrome_options)
+    return driver
 
 
 def convert_to_date_utc(category, pub_date):
     if type(pub_date) == str:
         if category == 'iam_school':
-            pub_date = datetime.strptime(pub_date, '%Y.%m.%d') # 문자열을 출력
-            print('before', pub_date)
+            pub_date = datetime.strptime(pub_date, '%Y.%m.%d')  # 문자열을 출력
             # # Set the time zone for KST
             # kst = pytz.timezone('Asia/Seoul')
 
             # Convert the KST datetime to UTC
             pub_date = pub_date.astimezone(pytz.utc)
-
-            print('after', pub_date)
-            print(type(pub_date))
-        elif category=='naver_blog':
-            pub_date = datetime.strptime(pub_date, '%Y. %m. %d. %H:%M') # 문자열을 출력
+        elif category == 'naver_blog':
+            pub_date = datetime.strptime(pub_date, '%Y. %m. %d. %H:%M')  # 문자열을 출력
             print('before', pub_date)
             pub_date = pub_date.astimezone(tz=pytz.utc)
             print('after', pub_date)
@@ -50,21 +58,21 @@ def convert_to_date_utc(category, pub_date):
     return pub_date
 
 
-
-def get_driver():
-    # 구글은 분당 4회이상 접근 허용하지 않기 때문에 sleep을 길게 준다.
-    # time.sleep(100)
-
-    # local chrome path
-    # path = "/usr/share/AISpera/merge_crawler/chromedriver"  # >> 나의 로컬 환경:/homejhhwang/updateWork/PIT/chromedriver vs 서버 환경:/usr/share/AISpera/google_news/chromedriver"
-    driver = webdriver.Chrome()
-    return driver
+def clean_text(text):
+    text = text.replace("\n", " ")  # 공백 제거
+    text = text.replace('\u200b', '')
+    text = text.replace("\'", "\'")
+    # text = re.sub(r"\'", "'", text)
+    text = re.sub(r"â\x80¦", "...", text)
+    # text = re.sub('[a-zA-Z]', '', text)
+    # text = re.sub('[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]','', text)
+    return text
 
 
 # 아이엠스쿨 기관 프로필 크롤링 완료
 def crawling_iam_school(iam_school_url):
     result = []
-    post_links = [] # 각 url 리스트
+    post_links = []  # 각 url 리스트
     # url = 'https://school.iamservice.net/organization/1674/group/2001892'
     url = iam_school_url
 
@@ -78,36 +86,46 @@ def crawling_iam_school(iam_school_url):
     try:
         driver = get_driver()
         driver.get(url)
-        driver.implicitly_wait(5) # 5초간 waiting
+        driver.implicitly_wait(5)  # 5초간 waiting
         soup = BeautifulSoup(driver.page_source, 'lxml')
         # print(soup)
         # print("=============================================================")
-        posts = soup.find_all('div', attrs={'class':'bx_cont'}) # a 요소 태그 다 추출
-        links = soup.find_all('a', attrs={'class':'btn_detail'}) # a 요소 태그 다 추출
+        posts = soup.find_all('div', attrs={'class': 'bx_cont'})  # a 요소 태그 다 추출
+        links = soup.find_all('a', attrs={'class': 'btn_detail'})  # a 요소 태그 다 추출
 
         for post in posts:
             post_one_dict = {}
             post_one_dict['title'] = post.find('h4', attrs={'class': 'tit_cont'}).get_text()
             post_one_dict['body'] = post.find('p', attrs={'class': 'desc'})
             post_one_dict['url'] = post.find('a', attrs={'class': 'btn_detail'})['href']
-            post_one_dict['published_date'] = convert_to_date_utc('iam_school', post.find('p', attrs={'class': 'txt_date type'}).find_all('span')[-1].get_text())
+            post_one_dict['published_datetime'] = convert_to_date_utc('iam_school', post.find('p', attrs={
+                'class': 'txt_date type'}).find_all('span')[-1].get_text())
             files = post.find_all('span', attrs={'class': 'name'})
-            post_one_dict['attatched_file_title'] = []
-            # print(post_one_dict['attatched_file_title'])
+            post_one_dict['attachment_list'] = []
             for f in files:
                 f = f.get_text()
-                post_one_dict['attatched_file_title'].append(f)
-            # print(post_one_dict['attatched_file_title'])
+                post_one_dict['attachment_list'].append(f)
+            # 아래로 변경 예정
+            # driver.get(post_one_dict['url'])
+            # # driver.implicitly_wait(5)  # 5초간 waiting
+            # soup = BeautifulSoup(driver.page_source, 'lxml')
+            # print("Encoding method :", soup.original_encoding)
+            # body = soup.find('div', attrs={'class': 'articleViewer'}).contents
+            # # temp_body = None
+            # # for b in body:
+            # #     temp_body += b
+            # # post_one_dict['body'] = temp_body  #clean_text(temp_body)
+            # post_one_dict['body'] = body  #clean_text(temp_body)
             result.append(post_one_dict)
 
         for i in result:
             print("i: ", i)
         print(result)
+        return result
 
     except Exception as ex:
         print("driver error: ", ex)
         print(traceback.format_exc())  # 에러스택 정보를 string으로 변환
-
 
     # url = 'https://school.iamservice.net/articles/view/135952777'
 
@@ -132,28 +150,17 @@ def crawling_iam_school(iam_school_url):
     # post_file_name = soup.find('div', attrs={'class': 'title'})
 
 
-def clean_text(text):
-    text = text.replace("\n", " ")  # 공백 제거
-    text = text.replace('\u200b', '')
-    text = text.replace("\'", "\'")
-    # text = re.sub(r"\'", "'", text)
-    text = re.sub(r"â\x80¦", "...", text)
-    # text = re.sub('[a-zA-Z]', '', text)
-    # text = re.sub('[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]','', text)
-    return text
-
-
 # 네이버 블로그 크롤링 완료
 def crawling_naver_blog():
     result = []
-    post_links = [] # 각 url 리스트
+    post_links = []  # 각 url 리스트
     url = 'https://blog.naver.com/PostList.nhn?blogId=sntjdska123&from=postList&categoryNo=51'
 
     try:
         response = requests.get(url)
         html = response.text
         soup = BeautifulSoup(html, 'lxml')
-        posts = soup.find('ul', attrs={'class':'thumblist'}).find_all('li', attrs={'class':'item'})
+        posts = soup.find('ul', attrs={'class': 'thumblist'}).find_all('li', attrs={'class': 'item'})
 
         # url 링크
         for post in posts:
@@ -162,7 +169,7 @@ def crawling_naver_blog():
 
         # 각 페이지 들어가서 반복할일!
         for post_url in post_links:
-            url = 'https://blog.naver.com'+post_url
+            url = 'https://blog.naver.com' + post_url
             response = requests.get(url)
             html = response.text
             soup = BeautifulSoup(html, 'lxml')
@@ -176,12 +183,10 @@ def crawling_naver_blog():
             post_one_dict['body'] = body
             print("=============================================================================")
 
-
             # todo: 선택1) html 본문 전체
             # print("body: ", body)
             # post_one_dict['body'] = body
             # print("=============================================================================")
-
 
             # todo: 선택2) html 본문 text **
             # # post_text_pattern = re.compile(r'se-component se-text [\w]') # 대안
@@ -200,14 +205,15 @@ def crawling_naver_blog():
 
             # todo: url 형태 맞추기
             post_one_dict['url'] = url
-            post_one_dict['published_date'] = convert_to_date_utc('naver_blog', soup.find('span', attrs={'class': 'se_publishDate pcol2'}).get_text())
+            post_one_dict['published_datetime'] = convert_to_date_utc('naver_blog', soup.find('span', attrs={
+                'class': 'se_publishDate pcol2'}).get_text())
 
             # todo: 첨부파일이 있는 블로그 글 찾기
             # files = post.find_all('span', attrs={'class': 'name'})
-            post_one_dict['attatched_file_title'] = []
+            post_one_dict['attachment_list'] = []
             # for f in files:
             #     f = f.get_text()
-            #     post_one_dict['attatched_file_title'].append(f)
+            #     post_one_dict['attachment_list'].append(f)
             result.append(post_one_dict)
 
         for i in result:
@@ -221,7 +227,7 @@ def crawling_naver_blog():
 
 def crawling_news():
     result = []
-    post_links = [] # 각 url 리스트
+    post_links = []  # 각 url 리스트
     url = 'http://feeds.bbci.co.uk/news/rss.xml'
 
     try:
@@ -248,13 +254,13 @@ def crawling_news():
 
             post_one_dict['url'] = post.find('link').get_text()
             post_one_dict['title'] = post.find('title').get_text()
-            post_one_dict['published_date'] = convert_to_date_utc('bbc_news' ,post.find('pubDate').get_text())
+            post_one_dict['published_datetime'] = convert_to_date_utc('bbc_news', post.find('pubDate').get_text())
             # files = post.find_all('span', attrs={'class': 'name'})
-            post_one_dict['attatched_file_title'] = []
+            post_one_dict['attachment_list'] = []
             # for f in files:
             #     f = f.get_text()
-            #     post_one_dict['attatched_file_title'].append(f)
-            # # print(post_one_dict['attatched_file_title'])
+            #     post_one_dict['attachment_list'].append(f)
+            # # print(post_one_dict['attachment_list'])
 
             # post_links.append(post.find('link').get_text())
             response = requests.get(post_one_dict['url'])
@@ -289,19 +295,41 @@ def crawling_news():
         print(traceback.format_exc())  # 에러스택 정보를 string으로 변환
 
 
-# def insert_data_db(crawled_data_list):
-#     Post.objects.
-#     return
+def insert_data_db(category, crawled_data_list):
+    # Post.objects.all()
+    item = crawled_data_list[0]
+    # for item in crawled_data_list:
+    #     print("item", item)
+        # print("item['url']", item['url'] + '  ' + str(type(item['url'])))
+        # print("item['title']", item['title'] + '  ' + str(type(item['title'])))
+        # # print("item['published_datetime']", item['published_datetime'] + '  ' + str(type(item['published_datetime'])))
+        # print("item['body']", item['body'] + '  ' + str(type(item['body'])))
+        # print("item['attachment_list']", ",".join(item['attachment_list']) + '  ' + str(type(",".join(item['attachment_list']))))
+        # print("category", category)
+
+    # if type(item[''])
+
+    Post(url=item['url'],
+         title=item['title'],
+         published_datetime=item['published_datetime'],
+         body=str(item['body']),
+         attachment_list=",".join(item['attachment_list']),
+         category=category).save()
+        # category=category).save()
+
+        # return
+    return
+
 
 if __name__ == '__main__':
-    iam_school_list = ['https://school.iamservice.net/organization/1674/group/2001892', 'https://school.iamservice.net/organization/19710/group/2091428']
+    iam_school_list = ['https://school.iamservice.net/organization/1674/group/2001892',
+                       'https://school.iamservice.net/organization/19710/group/2091428']
     for iam_school in iam_school_list:
-        crawling_iam_school(iam_school)
-    #
+        print("시작")
+        insert_data_db(category='iam_school', crawled_data_list=crawling_iam_school(iam_school))
+
     # naver_blog_list = ['https://blog.naver.com/PostList.nhn?blogId=sntjdska123&from=postList&categoryNo=51']
     # crawling_naver_blog()
 
     # crawling_news()
-
-    # todo: date time (timezone)
     # DB insert 하는 로직 (중복 방지)
--- nhn/models.py
+++ nhn/models.py
@@ -5,9 +5,9 @@ from django.db import models
 class Post(models.Model):
     url = models.URLField(verbose_name='게시물 URL')
     title = models.CharField(max_length=255, verbose_name='게시물 제목')
-    published_datetime = models.DateTimeField(verbose_name='게시물 발행일(작성일자)')
-    body = models.TextField(verbose_name='게시물 HTML')
-    attachment_list = models.TextField(verbose_name='첨부파일 이름 리스트')
+    published_datetime = models.DateTimeField(null=True, verbose_name='게시물 발행일(작성일자)')
+    body = models.TextField(null=True, verbose_name='게시물 HTML')
+    attachment_list = models.TextField(null=True,verbose_name='첨부파일 이름 리스트')
     category = models.CharField(max_length=255, verbose_name='게시물 수집 대상')
 
     class Meta:
